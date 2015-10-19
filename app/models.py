1d1934514b92eaa40067baaece7406bdc94c7793
yjamje <finallym@hotmail.com>
2015-10-19 17:13:01 +0800

    init

---
+++ app/models.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+from app import db
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+
+    def __str__(self):
+        return '<User %r>' % self.nickname
